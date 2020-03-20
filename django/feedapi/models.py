5214b64ea1604bead639cc87722ea5db057393c8
Kadir Akın Korkunç <kadiakinkorkunc@gmail.com>
2020-03-20 03:33:42 +0300

    initial models defined

--- django/feedapi/models.py
+++ django/feedapi/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from datetime import date
+from authapi.models import Member
 # Create your models here.
+
+
+class Post(models.Model):
+  title = models.CharField(max_length=100, null=False)
+  notes = models.CharField()
+  start_date = models.DateField(null=True)
+  end_date = models.DateField(null=True)
+  created_at = models.DateField(default=date.today)
+  owner_id = models.ForeignKey(Member, null = False, on_delete=models.SET_NULL)
+
+  def __str__(self):
+    return self.title
